7fc1440b1c2d448bf5f793019a8bdd9ccf5aee76
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2020-09-02 20:09:31 -0700

    Create main.java

---
+++ src/main/java/io/main.java
@@ -0,0 +1,13 @@
+package io;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+    @Override
+    public void onEnable() {
+    }
+    
+    @Override
+    public void onDisable() {
+    }
+}

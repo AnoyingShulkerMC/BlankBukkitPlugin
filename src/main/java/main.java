4ae8313b3842860bccaa1029b53019b91a1ace2d
AnoyingShulkerMC <44982417+AnoyingShulkerMC@users.noreply.github.com>
2020-09-02 20:07:22 -0700

    Update main.java

--- src/main/java/main.java
+++ src/main/java/main.java
@@ -4,10 +4,10 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public class main extends JavaPlugin {
     @Override
-    public void onEnable {
+    public void onEnable() {
     }
     
     @Override
-    public void onDisable {
+    public void onDisable() {
     }
 }
